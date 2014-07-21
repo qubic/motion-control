bb9108c42bf5022e9444cf35462f2bb4ab205fd5
qubicllj <qubicllj@gmail.com>
2014-07-22 00:50:51 +0800

    Basic feature has been implemented

---
+++ motion-control.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+    "github.com/go-martini/martini"
+    "github.com/martini-contrib/render"
+    "os"
+    "os/exec"
+    "log"
+    "net/http"
+)
+
+func main() {
+    motionPidFile := "/var/run/motion/motion.pid"
+    logFile := "/tmp/motion-control.log"
+    f, err := os.OpenFile(logFile, os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+    if err != nil {
+        log.Fatalf("error opening file: %v", err)
+    }
+    defer f.Close()
+    m := martini.Classic()
+    m.Map(log.New(f, "[martini]", log.LstdFlags))
+    m.Use(render.Renderer())
+    m.Get("/", func(r render.Render) {
+        if _, err := os.Stat(motionPidFile); err == nil {
+            r.HTML(200, "status", map[string]interface{}{"motionStatus": "running", "availableAction": "stop"})
+        } else {
+            r.HTML(200, "status", map[string]interface{}{"motionStatus": "not running", "availableAction": "start"})
+        } 
+    })
+    m.Get("/stop", func(res http.ResponseWriter, req *http.Request) {
+        cmd := exec.Command("service", "motion", "stop")
+        err := cmd.Run()
+        if err != nil {
+            log.Println(err)
+        } else {
+            log.Println("Stop motion from motion-control")
+        }
+        http.Redirect(res, req, "/", http.StatusSeeOther)
+    })
+    m.Get("/start", func(res http.ResponseWriter, req *http.Request) {
+        cmd := exec.Command("service", "motion", "start")
+        err := cmd.Run()
+        if err != nil {
+            log.Println(err)
+        } else {
+            log.Println("Stop motion from motion-control")
+        }
+        http.Redirect(res, req, "/", http.StatusSeeOther)
+    })
+    m.Run()
+}
+
